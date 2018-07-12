be6dac6be23e6bacd7b6e4370860cd9196f96352
chrisgou <gouyuanjunbest@163.com>
2018-07-12 21:19:01 +0800

    add vux

--- src/modules/mod1/views/About.vue
+++ src/modules/mod1/views/About.vue
@@ -1,5 +1,19 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <group>
+      <cell title="title" value="value"></cell>
+    </group>
   </div>
 </template>
+
+<script>
+import { Group, Cell } from "vux";
+
+export default {
+  components: {
+    Group,
+    Cell
+  }
+};
+</script>
--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,10 @@
-const multiPage = require('./multiPage.js')
 module.exports = {
-  baseUrl: process.env.NODE_ENV === 'production'
-    ? '/mayi_vue/'
-    : '/',
-  pages: multiPage
+  baseUrl: process.env.NODE_ENV === 'production' ? '/mayi_vue/' : '/',
+  pages: require('./multiPage.js'),
+  configureWebpack: config => {//只要这样配置，就可以正常的在你的项目中使用vux这个组件库了，但注意npm install vue-loader@14.2.2 -D
+    require('vux-loader').merge(config, {
+      options: {},
+      plugins: ['vux-ui']
+    })
+  }
 }
