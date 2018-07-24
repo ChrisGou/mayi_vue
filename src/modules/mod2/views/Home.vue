14f5537348231d89deaad97ca7ad1cb983476339
chrisgou <gouyuanjunbest@163.com>
2018-07-24 18:06:56 +0800

    delete logo.png&HelloWorld.vue

--- src/modules/mod2/views/Home.vue
+++ src/modules/mod2/views/Home.vue
@@ -1,18 +1,19 @@
 <template>
   <div class="home">
-    <img src="../assets/logo.png">
-    <HelloWorld msg="Vue.js Multi-Page App Mod2"/>
+    <!--<img src="../assets/logo.png">-->
+    <!--<HelloWorld msg="Vue.js Multi-Page App Mod2"/>-->
+    <p>mod2 home</p>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from "../components/HelloWorld.vue";
+// import HelloWorld from "../components/HelloWorld.vue";
 
 export default {
   name: "home",
-  components: {
-    HelloWorld
-  }
+  // components: {
+  //   HelloWorld
+  // }
 };
 </script>
